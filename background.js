e6fab31c1e48ecfdf5f864a8a8495429afdb7a80
PC201407301139\Administrator <picori@163.com>
2018-06-05 21:23:29 +0800

    no message

--- background.js
+++ background.js
@@ -465,7 +465,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
             }else if(coupon["ajax"]){
               ajax(coupon,next_minute);
             } 
-          },60 * 1000 - 2000);
+          },60 * 1000 - 1000);
         });
       });
     });
